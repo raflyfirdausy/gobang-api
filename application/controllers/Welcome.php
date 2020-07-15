ef35ac92a35471586d7ea80fb8897406098f9f7d
Rafli Firdausy Irawan <rafly.firdausy@gmail.com>
2020-07-15 23:35:16 +0700

    UBAH INDEX

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,26 +1,25 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+defined('BASEPATH') or exit('No direct script access allowed');
+require APPPATH . '/libraries/REST_Controller.php';
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
+use PhpParser\Node\Expr\Cast\String_;
+use Restserver\Libraries\REST_Controller;
+
+class Welcome extends REST_Controller
+{
+
+	public function __construct($config = 'rest')
 	{
-		// $this->load->view('welcome_message');
-		redirect("https://kejari-purwokerto.go.id");
+		parent::__construct($config);
+	}
+
+	public function index_get()
+	{
+		$this->response(array(
+			"status"        => true,
+			"respon_code"   => REST_Controller::HTTP_FORBIDDEN,
+			"respon_mess"   => "Forbidden",			
+		), REST_Controller::HTTP_OK);
 	}
 }
